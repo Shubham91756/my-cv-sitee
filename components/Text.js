1760b793ba496e673a671f6dda2c7ba3ebe77f41
Shubham91756 <ss8946@srmist.edu.in>
2025-07-08 13:19:29 +0530

    Update Text.js

--- components/Text.js
+++ components/Text.js
@@ -1,13 +1,13 @@
 import React, { memo } from "react"
 
-const Text = ({ title, ...props }) => {
+const Text = ({ title, placeholder, className }) => {
   return (
     <p
-      className={title && 'title--2'}
-      {...props}
+      className={`${title ? 'title--2' : ''} ${className || ''}`}
       contentEditable
       suppressContentEditableWarning
       spellCheck='false'
+      data-placeholder={placeholder}
     ></p>
   )
 }
